15390d4e5af8a3e7642c504a50426690256d5141
Bastien Guerry <bzg@gnu.org>
2025-03-18 00:07:29 +0100

    Update

--- src/rien.clj
+++ src/rien.clj
@@ -4,8 +4,13 @@
 ;; SPDX-License-Identifier: EPL-2.0
 ;; License-Filename: EPL-2.0.txt
 
+(ns bbtest)
+
 (def version "0.5.1")
 
 ;; (deps/add-deps '{:deps {org.clojars.askonomm/ruuter {:mvn/version "1.3.4"}}})
 
-(println "Rien!")
+(when (= *file* (System/getProperty "babashka.file"))
+  (println "Rien!"))
+
+
