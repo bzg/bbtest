c87776305af546f6f3a795bede81e3809f710819
Bastien Guerry <bzg@gnu.org>
2025-03-17 23:58:53 +0100

    src/rien.clj: New

---
+++ src/rien.clj
@@ -0,0 +1,11 @@
+#!/usr/bin/env bb
+
+;; Copyright (c) Bastien Guerry
+;; SPDX-License-Identifier: EPL-2.0
+;; License-Filename: EPL-2.0.txt
+
+(def version "0.5.1")
+
+;; (deps/add-deps '{:deps {org.clojars.askonomm/ruuter {:mvn/version "1.3.4"}}})
+
+(println "Rien!")
