069639c6126682eabe6d7066188e5faad05178c6
Bastien Guerry <bzg@gnu.org>
2025-03-18 00:16:53 +0100

    src/bbtest.clj: update

--- src/bbtest.clj
+++ src/bbtest.clj
@@ -1,12 +1,9 @@
 #!/usr/bin/env bb
 
-;; Copyright (c) Bastien Guerry
-;; SPDX-License-Identifier: EPL-2.0
-;; License-Filename: EPL-2.0.txt
+(ns bzg.bbtest)
 
-(ns bbtest)
-
-;; (deps/add-deps '{:deps {org.clojars.askonomm/ruuter {:mvn/version "1.3.4"}}})
+;; (defn -main [& args]
+;;   (println "Rien!"))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (println "Rien!"))
