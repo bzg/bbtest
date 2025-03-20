86532ced3681f45a20b3a0565cd256504c3a244d
Bastien Guerry <bzg@gnu.org>
2025-03-20 22:39:34 +0100

    update

--- bbtest.clj
+++ bbtest.clj
@@ -1,12 +1,11 @@
 #!/usr/bin/env bb
 
-(ns bbtest)
-
-(require '[babashka.cli :as cli]
-         '[babashka.deps :as deps]
-         '[babashka.http-client :as http]
-         '[cheshire.core :as json]
-         '[clojure.string :as str])
+(ns bbtest
+  (:require [babashka.cli :as cli]
+            [babashka.deps :as deps]
+            [babashka.http-client :as http]
+            [cheshire.core :as json]
+            [clojure.string :as str]))
 
 ;; Add the bblgum dependency
 (deps/add-deps '{:deps {io.github.lispyclouds/bblgum {:git/sha "aa1e2e494a1441eb26c25830d21b4458259112c3"}}})
